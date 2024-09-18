774dcd16a5e412244b17e9386174b942ee8f4355
RafaelCamposTXR <102463380+RafaelCamposTXR@users.noreply.github.com>
2024-09-18 13:36:34 -0300

    ajustando indentação

--- mainLoop.gd
+++ mainLoop.gd
@@ -22,30 +22,115 @@ var y = 2
 
 func _ready():
 	
-	var rede_neural = [
-	[
-		{"pesos": [0.5, -0.2], "bias": 0.1},
-		{"pesos": [0.3, 0.8], "bias": -0.4}
-	],
-	[
-		{"pesos": [0.7, 0.6], "bias": 0.2},
-		{"pesos": [-0.5, 0.9], "bias": 0.0}
+	# JSON string definida
+	var json_string = """
+	{
+		"layers": [
+		{
+		"weights": [
+			[0.367544247, 0.399710701, 0.562482995, 1.21584223],
+			[0.733857775, 1.63110209, -1.62498659, -1.09241287],
+			[-0.490230015, 0.277883485, 0.540120978, 0.34298549]
+		],
+		"biases": [-0.19923328, 2.29299105, -0.3737735],
+		"activation": "ReLU"
+		},
+		{
+		"weights": [
+			[-1.44311522, 1.60677038, 0.431788186],
+			[-0.302050927, 0.320649974, -0.0542225471]
+		],
+		"biases": [0.16481194, 1.43615104],
+		"activation": "ReLU"
+		}
 	]
-	]
-
+	}
+	"""
+	
+	# Parse o JSON para um dicionário
+	var json = JSON.new()
+	#var rede_neural = json.parse(json_string)
+	
+	
+	var rede_neural_dict = {
+		"layers": [
+			{
+				"weights": [
+					[0.367544247, 0.399710701, 0.562482995, 1.21584223],
+					[0.733857775, 1.63110209, -1.62498659, -1.09241287],
+					[-0.490230015, 0.277883485, 0.540120978, 0.34298549]
+				],
+				"biases": [-0.19923328, 2.29299105, -0.3737735],
+				"activation": "ReLU"
+			},
+			{
+				"weights": [
+					[-1.44311522, 1.60677038, 0.431788186],
+					[-0.302050927, 0.320649974, -0.0542225471]
+				],
+				"biases": [0.16481194, 1.43615104],
+				"activation": "ReLU"
+			}
+		]
+	}
+	
+	var rede_neural_array = [
+  [
+	[[0.367544247, 0.399710701, 0.562482995, 1.21584223]],  # weights da primeira camada
+	[-0.19923328, 2.29299105, -0.3737735],  # biases da primeira camada
+	"ReLU"  # activation da primeira camada
+  ],
+  [
+	[[-1.44311522, 1.60677038, 0.431788186]],  # weights da segunda camada
+	[0.16481194, 1.43615104],  # biases da segunda camada
+	"ReLU"  # activation da segunda camada
+  ]
+]
+	
 	var entradas_iniciais = [1.0, -1.0]
+	print(calcular_ativacoes(rede_neural_array,entradas_iniciais))
+	# Verifique se o dicionário tem a chave 'layers'
+	#if rede_neural_dados.has("layers"):
+		#var camadas = rede_neural_dados["layers"]
+		#
+		## Iterar sobre as camadas e acessar os valores com segurança
+		#for camada in camadas:
+			#if camada.has("weights") and camada.has("biases") and camada.has("activation"):
+				#var pesos = camada["weights"]
+				#var biases = camada["biases"]
+				#var ativacao = camada["activation"]
+				#
+				#print("Pesos: ", pesos)
+				#print("Biases: ", biases)
+				#print("Função de Ativação: ", ativacao)
+			#else:
+				#print("Erro: Camada não contém todos os dados necessários.")
+	#else:
+		#print("Erro: Dicionário não contém a chave 'layers'.")
+	#if rede_neural.error == OK:
+		#var dados = rede_neural.result  
+		#var camadas = dados["layers"] 
+		#
 
-	print("rede gerada = ", calcular_ativacoes(rede_neural, entradas_iniciais))
-	print(ReLU([0,1,1],[[3,2,2],[4,5,5],[5,3,3]],[5,4,3]))
+		#for camada in camadas:
+			#var ativacao = camada["activation"]
+			#print("Função de ativação da camada:", ativacao)
+	#else:
+		#print("Erro ao parsear JSON: ", rede_neural.error_string)
+
 	
-	var hidden_layer_output = ann0[0][2].call(inputs, ann0[0][0], ann0[0][1])
-	var output_layer_output = ann0[1][2].call(hidden_layer_output, ann0[1][0], ann0[1][1])
+
+	#print("rede gerada = ", calcular_ativacoes(rede_neural, entradas_iniciais))
+	#print(ReLU(1))
 	
+	#var hidden_layer_output = ann0[0][2].call(inputs, ann0[0][0], ann0[0][1])
+	#var output_layer_output = ann0[1][2].call(hidden_layer_output, ann0[1][0], ann0[1][1])
+	#
 	# Captura e armazena as informações da rede neural
-	network_info = store_neural_network_info(ann0, inputs, [hidden_layer_output], output_layer_output)
-	
-	# Salva as informações da rede neural em um arquivo JSON
-	save_network_info_to_json("res://informacoes_rede.json", network_info)
+	#network_info = store_neural_network_info(ann0, inputs, [hidden_layer_output], output_layer_output)
+	#
+	## Salva as informações da rede neural em um arquivo JSON
+	#save_network_info_to_json("res://informacoes_rede.json", network_info)
 
 
 	CriarLinha(Vector3(-2,2,0), Vector3(2,4,0))
@@ -116,14 +201,25 @@ func calcular_ativacoes(rede_neural: Array, entradas_iniciais: Array) -> Array:
 
 	for camada in rede_neural:
 		var ativacoes_camada = []
-		for i in range(len(camada["biases"])):
-			var z = somatorio_ponderado(entradas, camada["weights"][i], camada["biases"][i])
+		var pesos = camada[0]  # Acessa os pesos da camada
+		var biases = camada[1]  # Acessa os biases da camada
+		var ativacao_nome = camada[2]  # Acessa o nome da função de ativação
+		
+		# Obtém a função de ativação com base no nome
+		var func_ativacao = get_funcao_ativacao(ativacao_nome)
+		
+		for i in range(len(biases)):
+			if i >= pesos.size():
+				push_error("Erro: O índice do bias é maior que o tamanho da matriz de pesos.")
+				continue
 			
-			# Obtendo a função de ativação específica do neurônio
-			var func_ativacao = get_funcao_ativacao(camada["activation"])
+			# Certifique-se de que o tamanho das entradas corresponde ao número de pesos
+			if entradas.size() != pesos[i].size():
+				push_error("Erro: O tamanho das entradas não corresponde ao número de pesos.")
+				continue
 			
-			# Calculando a ativação usando a função especificada
-			var a = func_ativacao(z)
+			var z = somatorio_ponderado(entradas, pesos[i], biases[i])
+			var a = func_ativacao.call([z])  # Chama a função de ativação usando call()
 			ativacoes_camada.append(a)
 		
 		ativacoes.append(ativacoes_camada)
@@ -131,18 +227,39 @@ func calcular_ativacoes(rede_neural: Array, entradas_iniciais: Array) -> Array:
 	
 	return ativacoes
 
-# Função auxiliar para obter a função de ativação correta
-func get_funcao_ativacao(nome_funcao: String) -> Callable:
-	match nome_funcao:
+# Função fictícia para somatório ponderado
+func somatorio_ponderado(entradas, pesos, bias):
+	var resultado = bias
+	for i in range(len(entradas)):
+		resultado += entradas[i] * pesos[i]
+	return resultado
+
+# Função para obter a função de ativação baseada em um nome
+func get_funcao_ativacao(nome: String):
+	match nome:
 		"ReLU":
 			return ReLU
-#		"sigmoid":
-#			return funcao_sigmoid
-#		"tanh":
-#			return funcao_tanh
+		"Sigmoid":
+			return funcao_sigmoid
+		"tanh":
+			return funcao_tanh
 		_:
-			return funcao_default  # Função padrão caso o nome não corresponda a nenhuma função
+			return ReLU  # Função padrão
 
+# Funções de ativação exemplo
+
+
+
+
+
+func funcao_sigmoid(x):
+	return 1 / (1 + (2.718281828459045 ** -x)) 
+	
+func funcao_tanh(x):
+	var e_pos = 2.718281828459045 ** x  
+	var e_neg = 2.718281828459045 ** -x  
+	return (e_pos - e_neg) / (e_pos + e_neg)
+	
 func funcao_default(z: float) -> float:
 	print("Função de ativação desconhecida usada, retornando valor original.")
 	return z
@@ -262,11 +379,7 @@ func load_network_info_from_json(file_path):
 		print("Falha ao carregar informações da rede")
 	return network_info
 
-func somatorio_ponderado(entradas: Array, pesos: Array, bias: float) -> float:
-	var soma: float = 0.0
-	for i in range(len(entradas)):
-		soma += entradas[i] * pesos[i]
-	return soma + bias
+
 
 func funcao_de_ativacao(z: float) -> float:
 	return 1.0 / (1.0 + exp(-z))
